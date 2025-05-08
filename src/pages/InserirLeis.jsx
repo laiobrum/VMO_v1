7d47a8266f52639c6d17036f99a64bfd95259a47
laiobrum <laiobrum>
2025-05-08 10:12:14 -0300

    Atualização 3

--- src/pages/InserirLeis.jsx
+++ src/pages/InserirLeis.jsx
@@ -15,24 +15,35 @@ function InserirLeis() {
     const formatLaw = (e) => {
         e.preventDefault()
         setTexto(texto
-            .replace(/Art./g, "<span>Art.")//span antes do Art.
-            .replace(/(Art\. \d+\.)/g, "$1</span>")// /Span após o Art.
-            .replace(/(Art\. \d+-[A-Z]\.)/g, "$1</span>")// /span após "Art. 146-A."
-            .replace(/(§ \d+º)/g, "<span>$1")//span antes do § com º
-            .replace(/(§ \d+º)/g, "$1</span>")// /span após o § com º
-            .replace(/(§ \d+\.)/g, "<span>$1")//span antes do § com .
-            .replace(/(§ \d+\.)/g, "$1</span>")// /span após o § com .
-            .replace(/(?<!n)º/g, "º</span>")// após º, exceto se for "nº"
-            .replace(/^(\w)/gm, "<p><span>$1")// inclui span e p antes de qualquer parágrafo
-            .replace(/(.+)$/gm, "$1</p>")//adiciona p ao fim de todas as linhas
-            .replace(/ - /g, " - </span>")//adiciona /span após hífens
-            .replace(/ – /g, " - </span>")//adiciona /span após hífens - ele diferencia os hífens grandes dos pequenos
-            .replace(/([a-zA-Z])\)/g, "$1)</span>")// adiciona /span após alíneas 'a)'
-            .replace(/Parágrafo único./g, "Parágrafo único.</span>")//inclui /span após pu
+            //Espaços
             .replace(/^\s*\n/gm, "")//exclui parágrafos vazios
             .replace(/^\s*/gm, "")//exclui espaços vazios
-            .replace(/Redação dada pela Emenda Constitucional nº /g, "EC/")//Diminui texto p/ EC
-            .replace(/Incluído pela Emenda Constitucional nº /g, "EC/")//Diminui texto p/ EC
+            //Números ordinais
+            .replace(/(\d+)º/g, "$1.º")//Coloca na grafia correta "1.º"
+            //Art.
+            .replace(/Art\./g, "<span>Art.")//span antes do Art.
+            .replace(/(Art\. \d+\.º)/g, "$1</span>")
+            .replace(/(Art\. \d+\. )/g, "$1</span>")// /Span após o Art.
+            // .replace(/(Art\. \d+\.º)/g, "$1</span>")
+            // .replace(/(Art\. \d+º)/g, "$1.º</span>")// /Span após o Art. º 
+            .replace(/(Art\. \d+-[A-Z]\.)/g, "$1</span>")// /span após "Art. 146-A."
+            //§§
+            .replace(/(§ \d+)º/g, "$1.º")//arrumar o § 1.º
+            .replace(/(§ \d+\.º)/g, "<span>$1</span>")//span antes do § com º
+
+            .replace(/(§ \d+\. )/g, "<span>$1</span>")// /span após o § com º
+            
+            .replace(/Parágrafo único./g, "Parágrafo único.</span>")//inclui /span após pu
+            //<p> em todos os parágrafos
+            .replace(/^(\w)/gm, "<p><span>$1")// inclui span e p antes de qualquer parágrafo
+            .replace(/(.+)$/gm, "$1</p>")//adiciona p ao fim de todas as linhas
+            //Incisos:
+            .replace(/ - /g, " - </span>")//adiciona /span após hífens
+            .replace(/ – /g, " - </span>")//adiciona /span após hífens - ele diferencia os hífens grandes dos pequenos
+            //Alíneas
+            .replace(/([a-zA-Z])\)/g, "$1)</span>")// adiciona /span após alíneas 'a)'
+            //Textos recorrentes
+
         )
         return 
     }
