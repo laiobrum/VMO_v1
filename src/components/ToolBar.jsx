573a7c2d72dfe3af7f6485159559dcb33b1c2a11
laiobrum.ti <laiobrum.ti@gmail.com>
2025-07-02 21:23:22 +0000

    02.07 pt 2

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -10,10 +10,12 @@ import AlertMessage from './AlertMessage';
 
 const ToolBar = () => {
   const [highlightMode, setHighlightMode] = useState(false)
+  const [boldMode, setBoldMode] = useState(false)
+  const [underlineMode, setUnderlineMode] = useState(false)
   const [alertMsg, setAlertMsg] = useState(null)
 
   // MARCA-TEXTO Função que aplica o destaque à seleção atual
-  const handleHighlight = () => {
+  const handleTool = () => {
     const selection = window.getSelection()
     if (!selection || selection.isCollapsed) return
 
@@ -27,14 +29,22 @@ const ToolBar = () => {
     //Checa se contém qualquer <p> ou </p>
     const containsParagraph = /<\/?p>/i.test(html)
     if(containsParagraph) {
-      console.log("Você não pode marcar mais de um dispositivo de uma vez.")
       setAlertMsg("Você não pode marcar mais de um dispositivo de uma vez.")
       selection.removeAllRanges()
       return
     }
 
     const span = document.createElement('span')
-    span.className = 'yellowHL'
+    const toolClasses = {
+      highlightMode: 'yellowHL',
+      boldMode: 'boldTxt',
+      underlineMode: 'underlineTxt'
+    }
+    let classes = []
+    if(highlightMode) classes.push(toolClasses.highlightMode)
+    if(boldMode) classes.push(toolClasses.boldMode)
+    if(underlineMode) classes.push(toolClasses.underlineMode)
+    span.className = classes.join(' ')
     span.appendChild(range.extractContents())
     range.insertNode(span)
 
@@ -42,30 +52,41 @@ const ToolBar = () => {
   }
 
   // Liga/desliga o modo marcação
-  const toggleHighlightMode = () => {
-    setHighlightMode(prev => !prev)
+  const toggleTool = (tool) => {
+    switch (tool) {
+      case 'highlighter':
+          setHighlightMode(prev => !prev)
+        break;
+      case 'bold':
+          setBoldMode(prev => !prev)
+        break;
+      case 'underline':
+          setUnderlineMode(prev => !prev)
+      break;
+      default:
+        break;
+    }    
   }
 
   // Liga/desliga o listener global de seleção
   useEffect(() => {
-    if (highlightMode) {
-      document.addEventListener('mouseup', handleHighlight)
-    } else {
-      document.removeEventListener('mouseup', handleHighlight)
-    }
+    const isActive = highlightMode || boldMode || underlineMode
+    if (isActive) {
+      document.addEventListener('mouseup', handleTool)
+    } 
 
     return () => {
-      document.removeEventListener('mouseup', handleHighlight)
+      document.removeEventListener('mouseup', handleTool)
     }
-  }, [highlightMode])
+  }, [highlightMode, boldMode, underlineMode])
 
   return (
     <div className='toolbar'>
       <div className='toolContainer'>
       {/* ESTA TOOLBAR FUNCIONA PARA ACIONAR TODA A VIEW OU ENTÃO COMANDOS TOGGLE! */}
-        <button onClick={toggleHighlightMode} style={{ backgroundColor: highlightMode ? "#ffd" : "" }}><PiHighlighterFill /></button>
-        <button><ImBold /></button>
-        <button><MdFormatUnderlined /></button>
+        <button onClick={() => toggleTool('highlighter')} style={{ backgroundColor: highlightMode ? "#ffd" : "" }}><PiHighlighterFill /></button>
+        <button onClick={() => toggleTool('bold')} style={{ backgroundColor: boldMode ? "#d8d8ff" : "" }}><ImBold /></button>
+        <button onClick={() => toggleTool('underline')} style={{ backgroundColor: underlineMode ? "#d8d8ff" : "" }}><MdFormatUnderlined /></button>
         <button><PiEraserFill /></button>
         <button><BiSolidCommentEdit /></button>
         <button><CgFormatStrike /></button>
