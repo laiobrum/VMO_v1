e425c2e9fe3087786121e249197bce2eacf5d89c
laiobrum.ti <laiobrum.ti@gmail.com>
2025-07-10 21:19:41 +0000

    10.07 fim do dia

--- src/pages/InserirLeis.jsx
+++ src/pages/InserirLeis.jsx
@@ -28,23 +28,23 @@ function InserirLeis() {
     const fixOriginalTags = (e) => {
         e.preventDefault()
         let textoLimpo = texto
+            
+            //FICAR EM 1º - Extrai <a class="c927"> tags para não atrapalhar o id abaixo (é o ícone de julgados)
+            .replace(/<a[^>]*class="c927"[^>]*><\/a>/gi, '')
+            //FICAR 2º - Extrai as tags <a name=""> e coloca como id dos <p>. Remove as tags <a>, mantendo as que tem href
+            .replace(/<p([^>]*)>[\s\S]*?<a name="([^"]+)"[^>]*>[\s\S]*?<\/a>\s*([\s\S]*?)<\/p>/gi, '<p id="$2"$1>$3</p>')
+            //3º - extrai todos os atributos de <p>
+            .replace(/<p[^>]*id="([^"]+)"[^>]*>/gi, '<p id="$1">')
+
             //Tags
             .replace(/<(font|span|u|sup|i|small|b)[^>]*>/gi, '')//Remove <font> e <span>
             .replace(/<\/(font|span|u|sup|i|small|b)>/gi, '')//Remove </font> e </span>
-            .replace(/<(\w+)[^>]*>\s*<\/\1>/gi, '')//Remove tags vazias
-
-            //Atributos de tags
-            .replace(/<p[^>]*>/gi, '<p>')
 
             //Espaços e quebras
             .replace(/ {2,}/g, ' ')//Remove espaço
             .replace(/^\s*(&nbsp;)*\s*$/gm, '')//Remove espaço
             .replace(/&nbsp;/g, '')//Remove &nbsp; sozinho no meio do texto
             .replace(/\n{2,}/g, '\n') //Remove quebra de linha
-
-            //Extrai as tags <a name=""> e coloca como id dos <p>. Remove as tags <a>, mantendo as que tem href
-            .replace(/<p>\s*<a name="([^"]+)"\s*><\/a>\s*([\s\S]*?)<\/p>/gi, '<p id="$1">$2</p>')
-            
             
         setTexto(textoLimpo)
         return
